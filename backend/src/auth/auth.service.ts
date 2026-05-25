71288aa9349830ff1df27277f0e459524c2f585b
VitcaDiana <diana.ni.vitca@gmail.com>
2026-05-25 12:01:21 +0300

    register endpoint with bcrypt and validation

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async register(dto: RegisterDto) {
+    const existing = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (existing) {
+      throw new ConflictException('Email already in use');
+    }
+
+    //hash parola
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+    //salvez user
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+    return user;
+  }
+}
